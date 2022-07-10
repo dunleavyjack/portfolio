6cd109ab192f6687173fe7239a59a23af8baebb3
dunleavyjack <dunleavyjack@gmail.com>
2022-07-10 00:01:29 -0400

    Created parallax animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,54 @@
-import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import React from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import NameAnimation from '../components/NameAnimation';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Home = () => {
     return (
-        <main>
-            <NameAnimation />
-            <span
+        <Parallax pages={2} style={{ top: '0', left: '0' }}>
+            <ParallaxLayer
+                offset={0}
+                speed={2.5}
                 style={{
-                    position: 'absolute',
-                    bottom: '15px',
-                    width: '100vw',
-                    textAlign: 'center',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                 }}
             >
-                <Image
-                    src="/assets/images/down-arrow.svg"
-                    alt="Down"
-                    width={30}
-                    height={30}
-                />
-            </span>
+                <NameAnimation />
+                <h3 className={styles.scrollText}>SCROLL</h3>
+            </ParallaxLayer>
 
-            <section className={styles.intro}></section>
-        </main>
+            <ParallaxLayer
+                offset={1}
+                speed={2}
+                style={{ backgroundColor: 'black' }}
+            />
+
+            <ParallaxLayer
+                offset={1}
+                speed={0.5}
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    textAlign: 'center',
+                    alignItems: 'center',
+                    color: 'gray',
+                }}
+            >
+                <h1 style={{ color: 'white' }}>Nice to meet you.</h1>
+                <h2 style={{ color: 'white' }}>
+                    Im a full-stack developer building responsive, professional,
+                    and creative web applications using React, Redux, Figma, and
+                    Node.js.
+                </h2>
+                <h2 style={{ color: 'white' }}>
+                    I try to be creative, efficient, and design-oriented.
+                </h2>
+            </ParallaxLayer>
+        </Parallax>
     );
 };
 
