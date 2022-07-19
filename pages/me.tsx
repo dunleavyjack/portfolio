560de80c850eb20f33193c90540fe4436b5abe87
dunleavyjack <dunleavyjack@gmail.com>
2022-07-18 20:57:24 -0400

    Sample pages

---
+++ pages/me.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Me: NextPage = () => {
+    return (
+        <div>
+            <h1>this is the me page</h1>
+        </div>
+    );
+};
+
+export default Me;
---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Projects: NextPage = () => {
+    return (
+        <div>
+            <h1>projects page</h1>
+        </div>
+    );
+};
+
+export default Projects;
